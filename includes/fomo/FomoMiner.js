b4b2a26e8c4c180835a4de30513a499f6b810978
Souging <150511278+Souging@users.noreply.github.com>
2024-11-16 00:27:41 +0800

    Update FomoMiner.js

--- includes/fomo/FomoMiner.js
+++ includes/fomo/FomoMiner.js
@@ -394,7 +394,7 @@ export default class FomoMiner {
         suiMasterParams.privateKey = this._key;
         const suiMaster = new SuiMaster(suiMasterParams);
         await suiMaster.initialize();
-        const processCount =  10;
+        const processCount =  50;
     //const processCount = Math.floor(txs.length / 10) * 10;
     for (let i = 0; i < processCount; i++) {
         let args = [
